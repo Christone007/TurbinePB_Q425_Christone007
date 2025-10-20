384836cacdcceeaf5f14390c0f0e989cdb377687
Christone007 <exellentemy@gmail.com>
2025-10-20 07:07:06 +0100

    Add random number generation

--- RNG_Game/guessing_game/src/main.rs
+++ RNG_Game/guessing_game/src/main.rs
@@ -1,9 +1,19 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
-    let mut guess: String = String::from("start");
+    let mut guess: String = String::from("");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    
 
-    io::stdin().read_line(&mut guess);
+    println!("");
+
+    let res = io::stdin().read_line(&mut guess);
+
+    match res {
+        Err(a) => println!("{a}"),
+        Ok(a) => println!("{a}"),
+    }
 
     println!("The guess is {guess}");
 }
